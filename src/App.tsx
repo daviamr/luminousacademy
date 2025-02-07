04c8bf54b3942662c33709b988d8c188c4821ee6
Davi Amorim <daviamorim_sz@live.com>
2025-02-07 19:08:50 -0300

    correcao overflow-x

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import './global.css'
 function App() {
   return (
     <>
-      <div className='relative'>
+      <div className='relative overflow-x-hidden'>
         <NewHeader/>
 
         {/* main */}
